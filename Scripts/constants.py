49bd5b57a516849489ef641671d1415f06766090
alfonsogijon <alfonso.gijon2@gmail.com>
2023-07-24 12:51:11 +0200

    Added MM82 wind turbine constant parameters

---
+++ Scripts/constants.py
@@ -0,0 +1,10 @@
+# Constants of the model corresponding to a Senvion MM82 turbine
+
+""" 
+R = 46.0
+A = 6720.0
+"""
+R = 41.0       # rotor radius (m)
+A = 5281.0     # swept area   (m^2)
+gear = 106.0   # gear ratio   
+rho = 1.225    # air density  (kg/m^3)
